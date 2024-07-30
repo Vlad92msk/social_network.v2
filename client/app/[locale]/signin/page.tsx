5faa14a93252a33939b36aa759ea5ea4dea955aa
VladFirsov <firsovvs@answer-42.ru>
2024-07-30 19:51:35 +0500

    продолжаю разработку

--- client/app/[locale]/signin/page.tsx
+++ client/app/[locale]/signin/page.tsx
@@ -1,35 +1,42 @@
 import { getTranslations } from 'next-intl/server'
-import { Text } from '@ui/common/Text/Text'
-import { HeaderMenu } from 'app/[locale]/signin/components/HeaderMenu'
+import { Image } from '@ui/common/Image'
+import { Text, TextPropsFontSize } from '@ui/common/Text/Text'
 import { makeCn } from '@utils/others'
+import { HeaderMenu } from 'app/[locale]/signin/components/HeaderMenu'
 import { GoogleSignIn } from './components'
 import style from './page.module.scss'
 
 const cn = makeCn('Signin', style)
 
 export default async function SignInPage() {
+  const fs: TextPropsFontSize = {
+    xl: '28',
+    xs: '14',
+    es: '8',
+  }
+
   const t = await getTranslations()
   return (
     <>
+      <Image className={cn('BckImage')} alt="bck" width={1800} height={1800} src="base/auth" />
       <HeaderMenu />
       <main className={cn()}>
         <section className={cn('Salutation')}>
-          <Text
-            fs={{
-              xl: '44',
-              xs: '38',
-              sm: '20',
-              es: '8',
-            }}
-            letterSpacing={0.03}
-          >
-            {t('Auth.description').split('|').map((part, index, array) => (
-              <>
-                {part}
-                {index < array.length - 1 && <br />}
-              </>
-            ))}
+          <Text fs={fs} letterSpacing={0.03}>
+            Привет! Меня зовут {' '}
           </Text>
+          <Text fs={fs} className={cn('MyName')} weight="bold">Влад</Text>
+          <br />
+          <Text fs={fs}>Я </Text>
+          <Text fs={fs} className={cn('MyPosition')} weight="bold">frontend-developer</Text>
+          <br />
+          <br />
+          <Text fs={fs}>А это мой учебный проект</Text>
+          <br />
+          <Text fs={fs}>Здесь я экспериментирую с подходами/технологиями и т.д.</Text>
+          <br />
+          <br />
+          <Text fs={fs}>Занимаюсь им по мере свободного времени</Text>
         </section>
         <section className={cn('Enter')}>
           <Text className={cn('EnterText')} fs="14">
