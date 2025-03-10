fdba2c0f1c9a64fa9d04a5c0b654650f7276e344
VladFirsov <firsovvs@answer-42.ru>
2025-03-10 21:24:16 +0500

    возвращаю код Компонента обратно

--- client/app/[locale]/[userId]/(content)/profile/_components/AboutMe/AboutMe.tsx
+++ client/app/[locale]/[userId]/(content)/profile/_components/AboutMe/AboutMe.tsx
@@ -1,86 +1,132 @@
 'use client'
 
-import { PokemonDetails, pokemonEndpoints } from '@ui/modules/synapse/examples/example1'
-import { useCallback, useState } from 'react'
-// import { PokemonDetails, pokemonApi, pokemonEndpoints } from '@ui/modules/synapse/services/api/examples/pokemon-api'
+import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { AddedFile, useProfile } from '@hooks'
+import { Spinner } from '@ui/common/Spinner'
+import { createZustandContext } from '@utils/client'
 import { cn } from './cn'
+import {
+  Banner, ButtonEdit, Company, Information, Name, Position, Univercity,
+} from './elements'
+import { userInfoApi } from '../../../../../../../store/api'
+import { UserPageProps } from '../../page'
 
-export function PokemonCard() {
-  const [currentPokemon, setCurrentPokemon] = useState<PokemonDetails | undefined>(undefined)
-
-  const onPokemon = useCallback(async (id: number) => {
-    const request = pokemonEndpoints.getPokemonById.request({ id })
-
-
-    request.subscribe((state) => {
-      console.log('State:', state)
-      switch (state.status) {
-        case 'idle': {
-          console.log('запрос неактивен')
-          break
-        }
-        case 'loading': {
-          console.log('запрос loading')
-          break
-        }
-        case 'success': {
-          console.log('запрос success')
-          setCurrentPokemon(state.data)
-          break
-        }
-        case 'error': {
-          console.log('запрос error')
-          break
-        }
-      }
-    }, {autoUnsubscribe: true})
-    const dfwf = await request.wait()
-    console.log('d', dfwf)
-    request.waitWithCallbacks({
-      idle: () => {},
-      loading: () => {},
-      success: (data, request) => {},
-      error: () => {},
-    }).catch(console.error).finally()
-
-
-  }, [])
-
-  // console.log('currentPokemon', currentPokemon)
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <h2>{currentPokemon?.name}</h2>
-      <img src={currentPokemon?.sprites.front_default} alt={currentPokemon?.name} />
-      <div>
-        Types:
-        {' '}
-        {currentPokemon?.types.map((t) => t.type.name).join(', ')}
-      </div>
-      <div>
-        <button
-          onClick={() => onPokemon((currentPokemon?.id || 0) + 1)}
-        >
-          Next
-        </button>
-        <span>
-          Pokemon #
-          {currentPokemon?.id}
-        </span>
-        <button
-          onClick={() => onPokemon((currentPokemon?.id || 0) - 1)}
-        >
-          Previous
-        </button>
-      </div>
-    </div>
-  )
+// Поля, которые могут быть отредактированы
+export interface AboutMeContextChangeState {
+  information?: string
+  position?: string
+  university?: string
+  company?: string
+  banner?: string
+  name?: string
+  bannerUploadFile?: AddedFile
+  imageUploadFile?: AddedFile
 }
 
-export function AboutMe(props) {
+interface PublicationContextState {
+  isChangeActive?: boolean
+  changeState?: AboutMeContextChangeState
+  status?: 'view' | 'reset' | 'approve'
+}
+
+const initialState: PublicationContextState = {
+  isChangeActive: false,
+  status: 'view',
+  changeState: {},
+}
+
+export const {
+  contextZustand,
+  useZustandSelector: useAboutMeCtxSelect,
+  useZustandDispatch: useAboutMeCtxUpdate,
+} = createZustandContext(initialState)
+
+export interface AboutMeProps {
+}
+
+export const AboutMe = contextZustand<AboutMeProps, PublicationContextState>((props) => {
+  // @ts-ignore
+  const params = useParams<UserPageProps['params']>()
+  const aboutMeUpdate = useAboutMeCtxUpdate()
+
+  const { profile, isLoading } = useProfile()
+
+  const handleClickFriend = (id: string) => {
+    console.log(`Переходим к пользователю ${id}`)
+  }
+  /**
+   * Получаем инф о пользователе если это не текущий пользователь
+   * (чтобы отображать инф о пользователе из URL)
+   */
+  const { data: gotUserProfile, status } = userInfoApi.useGetUsersQuery(
+    //@ts-ignore
+    { public_id: params.userId },
+    //@ts-ignore
+    { skip: profile?.user_info.public_id === params.userId },
+  )
+  const [updateUser, { isError }] = userInfoApi.useUpdateUserMutation()
+
+  useEffect(() => {
+    if (isError) {
+      aboutMeUpdate(() => ({ isChangeActive: false, status: 'reset', changeState: {} }))
+    }
+  }, [aboutMeUpdate, isError])
+
+  const handleSubmit = (data?: AboutMeContextChangeState) => {
+    if (!data) return
+
+    const formData = new FormData()
+    if (data.name) formData.append('name', data.name)
+    if (data.university) formData.append('about_info[study]', data.university)
+    if (data.company) formData.append('about_info[working]', data.company)
+    if (data.position) formData.append('about_info[position]', data.position)
+    if (data.information) formData.append('about_info[description]', data.information)
+
+    if (data.imageUploadFile && data.imageUploadFile.blob instanceof File) {
+      formData.append('profile_image', data.imageUploadFile.blob, data.imageUploadFile.name)
+    }
+
+    if (data.bannerUploadFile && data.bannerUploadFile.blob instanceof File) {
+      formData.append('banner_image', data.bannerUploadFile.blob, data.bannerUploadFile.name)
+    }
+
+    updateUser({
+      // @ts-ignore
+      body: formData,
+    })
+  }
+
+  const [currentUser, setCurrentUser] = useState(profile?.user_info)
+
+  useEffect(() => {
+    //@ts-ignore
+    if (profile?.user_info.public_id !== params.userId && status === 'fulfilled' && gotUserProfile?.length) {
+      // @ts-ignore
+      setCurrentUser(gotUserProfile[0])
+    }
+  }, [gotUserProfile, params, profile, status])
+
   return (
     <div className={cn()}>
-      {/* <MyTest /> */}
-        <PokemonCard />
+      {
+        isLoading ? <Spinner /> : currentUser && (
+          <>
+            <ButtonEdit onSubmit={handleSubmit} />
+            <Banner
+              contacts={[]}
+              image={currentUser.profile_image}
+              bunner_image={currentUser.about_info.banner_image}
+              onClickUser={handleClickFriend}
+            />
+            <Name name={currentUser.name} />
+            <Univercity university={currentUser.about_info.study} />
+            <Position position={currentUser.about_info.position} />
+            <Company company={currentUser.about_info.working} />
+            <Information information={currentUser.about_info.description} />
+          </>
+        )
+      }
     </div>
   )
-}
+})
